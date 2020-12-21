c970475570c538ae23180b1b9b8df291cdc2b20f
Kachii <Kacheokcheong@gmail.com>
2020-12-21 21:48:39 +0000

    refactored commands for fpl and youtube

--- kach_api_endpoints/management/commands/repop_fpl_data.py
+++ kach_api_endpoints/management/commands/repop_fpl_data.py
@@ -1,23 +1,52 @@
+import requests
+from django.core.management.base import BaseCommand
 import json
 import os
-from django.core.management.base import BaseCommand
 from kach_api_endpoints.api_list.fpl_api.fpl_model import MatchDay, Player
 from kach_api_endpoints.management.repoppers.fpl_repoppers import create_fpl_data
 
-FPL_DATA_DIR = os.getcwd() + '/kach_api_endpoints/data/fpl/players/{}Data.json'
-LEAGUE_DATA = os.getcwd() + '/kach_api_endpoints/data/fpl/league/leagueData.json'
+league_id = 357383  # Insert League Id HERE
+
+OUTFILE_LOCATION = os.getcwd() + "/kach_api_endpoints/data/fpl/"
+
+ENDPOINT_URL = "https://fantasy.premierleague.com/api"
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
+        league_response = requests.get(f"{ENDPOINT_URL}/leagues-classic/{league_id}/standings/").json()
+
+        with open(f"{OUTFILE_LOCATION}league/leagueData.json", 'w', encoding='utf8') as json_file:
+            json_file.write(json.dumps(league_response, indent=4, ensure_ascii=False))
+
+        print(f"Successfully prepped leagueData.json")
+
+        with open(f"{OUTFILE_LOCATION}league/leagueData.json", 'r') as json_file:
+            data = json.load(json_file)
+
+            print("prepping players data...")
+
+            for player in data["standings"]["results"]:
+                match_response = requests.get(f"{ENDPOINT_URL}/entry/{player['entry']}/history/").json()
+
+                player_response = requests.get(f"{ENDPOINT_URL}/entry/{player['entry']}/").json()
+
+                with open(f"{OUTFILE_LOCATION}/players/{player['entry']}Data.json", 'w', encoding='utf8') as json_file:
+                    json_file.write(json.dumps((match_response, player_response), indent=4, ensure_ascii=False))
+
+        print("Successfully prepped players data!")
+
         MatchDay.objects.all().delete()
         Player.objects.all().delete()
 
-        with open(LEAGUE_DATA, 'r') as json_file:
+        print("Repopping player and match data...")
+
+        with open(f"{OUTFILE_LOCATION}league/leagueData.json", 'r') as json_file:
             data = json.load(json_file)
 
             for player in data["standings"]["results"]:
-                create_fpl_data(FPL_DATA_DIR.format(player["entry"]))
+                create_fpl_data(f"{OUTFILE_LOCATION}players/{player['entry']}Data.json")
 
         match_days = MatchDay.objects.all()
         players_list = Player.objects.all()
@@ -28,4 +57,4 @@ class Command(BaseCommand):
                     correct_player = Player.objects.get(player_id=match.player_id)
                     correct_player.matches.add(match)
 
-        print("repop successful!")
+        print("Repop successful!")
--- kach_api_endpoints/management/commands/repop_youtube.py
+++ kach_api_endpoints/management/commands/repop_youtube.py
@@ -1,37 +1,60 @@
+import requests
 from django.core.management.base import BaseCommand
-import os
 import json
+import os
 # Models
 from kach_api_endpoints.api_list.youtube_api.youtube_model import YoutubeVideo, YoutubePlaylist
 from kach_api_endpoints.management.repoppers.youtube_video_repoppers import create_new_youtube_videos
 
+OUTFILE_LOCATION = os.getcwd() + "/kach_api_endpoints/data/youtube"
 
-YOUTUBE_DATA_DIR = os.getcwd() + "/kach_api_endpoints/data/youtube/"
+DATA_ENDPOINT_URL = "https://www.googleapis.com/youtube/v3/playlistItems?playlistId={}&key=AIzaSyAC-vA8irZClKOO8zVMv4wyF3URfTe6HMA&part=snippet,id&order=date&maxResults=20"
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        """" Deletes all Videos/Playlists and repopulates them the the prep_youtube data"""
+        """" Hits the youtube api and returns all playlists in the data folder """
+
+        with open(f"{OUTFILE_LOCATION}/playlistData.json", 'r') as json_file:
+            data = json.load(json_file)
+
+        print("Gathering videos from playlist data...")
+
+        for endpoint in data:
+            response = requests.get(DATA_ENDPOINT_URL.format(endpoint["playlist_id"])).json()
+
+            with open(f"{OUTFILE_LOCATION}/playlists/{endpoint['playlist_name']}.json", 'w',
+                      encoding='utf8') as json_file:
+                json_file.write(
+                    json.dumps(response, indent=4, ensure_ascii=False)
+                )
+
+        print("Successfully gathered playlists")
+
         YoutubeVideo.objects.all().delete()
         YoutubePlaylist.objects.all().delete()
 
-        with open(f"{YOUTUBE_DATA_DIR}playlistData.json", 'r') as json_file:
+        with open(f"{OUTFILE_LOCATION}/playlistData.json", 'r') as json_file:
             repop_data = json.load(json_file)
 
         for file in repop_data:
-            create_new_youtube_videos(YOUTUBE_DATA_DIR + f"playlists/{file['playlist_name']}.json")
-            print(f"Successfully added videos from {file['playlist_name']}.json")
+            create_new_youtube_videos(f"{OUTFILE_LOCATION}/playlists/{file['playlist_name']}.json")
+
+        print("Successfully added videos from playlists!")
 
         for playlist in repop_data:
             YoutubePlaylist(
                 playlist_name=playlist["playlist_name"],
                 playlist_id=playlist["playlist_id"]
             ).save()
-            print(f"Successfully created {playlist['playlist_name']} playlist")
+
+        print("Successfully created all playlist models")
 
         all_videos = YoutubeVideo.objects.all()
         all_playlists = YoutubePlaylist.objects.all()
+
+        print("Adding videos to correct playlists...")
 
         for video in all_videos:
             for playlist in all_playlists:
