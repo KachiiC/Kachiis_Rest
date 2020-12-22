43ae948819570b5536d729ae4975f4af5f849b62
Kachii <Kacheokcheong@gmail.com>
2020-12-22 00:17:20 +0000

    change card model

--- kach_backend_endpoints/backend_list/cards_api/card_model.py
+++ kach_backend_endpoints/backend_list/cards_api/card_model.py
@@ -5,4 +5,4 @@ class Card(models.Model):
     code = models.CharField(max_length=2)
     value = models.CharField(max_length=5)
     image = models.CharField(max_length=45)
-    suit = models.CharField(max_length=7)
+    suit = models.CharField(max_length=8)
