8177b6fec9b67281e7d4ed04cf376c99273f1415
Kachii <Kacheokcheong@gmail.com>
2020-12-22 00:22:55 +0000

    edited max length for cards model

--- kach_backend_endpoints/backend_list/cards_api/card_model.py
+++ kach_backend_endpoints/backend_list/cards_api/card_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Card(models.Model):
-    code = models.CharField(max_length=2)
-    value = models.CharField(max_length=5)
+    code = models.CharField(max_length=5)
+    value = models.CharField(max_length=20)
     image = models.CharField(max_length=45)
     suit = models.CharField(max_length=8)
