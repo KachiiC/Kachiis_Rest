485775e5e489baa846b3e7fc7860e1283a7ffe99
Kachii <Kacheokcheong@gmail.com>
2021-01-19 19:12:16 +0000

    remove repop script

--- kach_api_endpoints/management/commands/repop_instagram.py
+++ kach_api_endpoints/management/commands/repop_instagram.py
@@ -12,27 +12,27 @@ OUTFILE_LOCATION = os.getcwd() + "/kach_api_endpoints/data/instagram/instagramDa
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("Pulling data from API...")
-
-        url = "https://instagram-data1.p.rapidapi.com/user/feed"
-
-        querystring = {"username": "basically_mma"}
-
-        headers = {
-            'x-rapidapi-key': "985371e109mshb5666c0424d5dcfp1b7485jsndf2afe5a3591",
-            'x-rapidapi-host': "instagram-data1.p.rapidapi.com"
-        }
-
-        insta_response = requests.request(
-            "GET", url, headers=headers, params=querystring
-        ).json()
-
-        with open(OUTFILE_LOCATION, 'w', encoding='utf8') as json_file:
-            json_file.write(
-                json.dumps(insta_response, indent=4, ensure_ascii=False)
-            )
-
-        print("Data successfully pulled from API...")
+        # print("Pulling data from API...")
+        #
+        # url = "https://instagram-data1.p.rapidapi.com/user/feed"
+        #
+        # querystring = {"username": "basically_mma"}
+        #
+        # headers = {
+        #     'x-rapidapi-key': "985371e109mshb5666c0424d5dcfp1b7485jsndf2afe5a3591",
+        #     'x-rapidapi-host': "instagram-data1.p.rapidapi.com"
+        # }
+        #
+        # insta_response = requests.request(
+        #     "GET", url, headers=headers, params=querystring
+        # ).json()
+        #
+        # with open(OUTFILE_LOCATION, 'w', encoding='utf8') as json_file:
+        #     json_file.write(
+        #         json.dumps(insta_response, indent=4, ensure_ascii=False)
+        #     )
+        #
+        # print("Data successfully pulled from API...")
 
         Post.objects.all().delete()
 
