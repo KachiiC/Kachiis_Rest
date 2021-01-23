7dc042ac9c170315b1e736ff313a46db33c4b2b1
Kachii <Kacheokcheong@gmail.com>
2021-01-23 19:08:52 +0000

    edit instagram repop

--- kach_api_endpoints/api_list/instagram_api/instagram_views.py
+++ kach_api_endpoints/api_list/instagram_api/instagram_views.py
@@ -17,20 +17,20 @@ class InstagramPostApiView(views.APIView):
 
     @method_decorator(cache_page(60 * 60 * 2))
     def get(self, request):
-        url = "https://instagram-data1.p.rapidapi.com/user/feed"
+        url = "https://instagram40.p.rapidapi.com/account-medias"
 
         querystring = {
-            "username": "basically_mma"
+            "userid": "32817560066",
+            "first": "36",
+            "after": "QVFDOGV6dGFtQnJXdnZ0a1FuMkFLSjRHYjdWMEdTTFltMkZpd1FvcUxuQXZ6bDJFVnpKRzFYU3RMSUoyNXluOXFZUVZ3dG1YM3NSTEJqMVI3TTBKM0ZTNg=="
         }
 
         headers = {
             'x-rapidapi-key': "985371e109mshb5666c0424d5dcfp1b7485jsndf2afe5a3591",
-            'x-rapidapi-host': "instagram-data1.p.rapidapi.com"
+            'x-rapidapi-host': "instagram40.p.rapidapi.com"
         }
 
-        insta_response = requests.request(
-            "GET", url, headers=headers, params=querystring
-        )
+        insta_response = requests.request("GET", url, headers=headers, params=querystring).json()
 
         with open(OUTFILE_LOCATION, 'w', encoding='utf8') as json_file:
             json_file.write(
--- kach_api_endpoints/management/commands/repop_instagram.py
+++ kach_api_endpoints/management/commands/repop_instagram.py
@@ -12,28 +12,31 @@ OUTFILE_LOCATION = os.getcwd() + "/kach_api_endpoints/data/instagram/instagramDa
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # print("Pulling data from API...")
-        #
-        # url = "https://instagram40.p.rapidapi.com/account-medias"
-        #
-        # querystring = {"userid": "32817560066", "first": "36",
-        #                "after": "QVFDOGV6dGFtQnJXdnZ0a1FuMkFLSjRHYjdWMEdTTFltMkZpd1FvcUxuQXZ6bDJFVnpKRzFYU3RMSUoyNXluOXFZUVZ3dG1YM3NSTEJqMVI3TTBKM0ZTNg=="}
-        #
-        # headers = {
-        #     'x-rapidapi-key': "985371e109mshb5666c0424d5dcfp1b7485jsndf2afe5a3591",
-        #     'x-rapidapi-host': "instagram40.p.rapidapi.com"
-        # }
-        #
-        # insta_response = requests.request("GET", url, headers=headers, params=querystring).json()
-        #
-        # with open(OUTFILE_LOCATION, 'w', encoding='utf8') as json_file:
-        #     json_file.write(
-        #         json.dumps(insta_response, indent=4, ensure_ascii=False)
-        #     )
-        #
-        # print("Data successfully pulled from API...")
-        #
-        # Post.objects.all().delete()
+        print("Pulling data from API...")
+
+        url = "https://instagram40.p.rapidapi.com/account-medias"
+
+        querystring = {
+            "userid": "32817560066",
+            "first": "36",
+            "after": "QVFDOGV6dGFtQnJXdnZ0a1FuMkFLSjRHYjdWMEdTTFltMkZpd1FvcUxuQXZ6bDJFVnpKRzFYU3RMSUoyNXluOXFZUVZ3dG1YM3NSTEJqMVI3TTBKM0ZTNg=="
+        }
+
+        headers = {
+            'x-rapidapi-key': "985371e109mshb5666c0424d5dcfp1b7485jsndf2afe5a3591",
+            'x-rapidapi-host': "instagram40.p.rapidapi.com"
+        }
+
+        insta_response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+        with open(OUTFILE_LOCATION, 'w', encoding='utf8') as json_file:
+            json_file.write(
+                json.dumps(insta_response, indent=4, ensure_ascii=False)
+            )
+
+        print("Data successfully pulled from API...")
+
+        Post.objects.all().delete()
 
         print("Repopping Insta Data...")
 
@@ -41,4 +44,5 @@ class Command(BaseCommand):
 
         print("Successfully prepped Insta!")
 
-        print(Post.objects.all().count())
+        total_posts = Post.objects.all().count()
+        print(f"{total_posts} posts in total")
