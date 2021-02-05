db5fa8190d7be8464c56aa310da77ec5503eae08
Kachii <Kacheokcheong@gmail.com>
2021-02-05 17:06:58 +0000

    added atomic = False

--- kach_api_endpoints/migrations/0013_chip_chip_owner.py
+++ kach_api_endpoints/migrations/0013_chip_chip_owner.py
@@ -9,6 +9,8 @@ class Migration(migrations.Migration):
         ('kach_api_endpoints', '0012_auto_20210129_1725'),
     ]
 
+    atomic = False
+
     operations = [
         migrations.AddField(
             model_name='chip',
