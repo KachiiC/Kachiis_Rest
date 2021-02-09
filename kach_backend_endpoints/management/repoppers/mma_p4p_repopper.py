216ec56f937efc9bbe647397c119742ae5441de0
Kachii <Kacheokcheong@gmail.com>
2021-02-09 22:59:53 +0000

    completed p4p command

--- kach_backend_endpoints/management/repoppers/mma_p4p_repopper.py
+++ kach_backend_endpoints/management/repoppers/mma_p4p_repopper.py
@@ -2,7 +2,7 @@ from kach_backend_endpoints.backend_list.mma_divisions.mma_divisions_model impor
 from kach_backend_endpoints.backend_list.mma_fighters.mma_fighter_model import Fighter
 from .mma_fighter_repoppers import create_mma_fighter
 
-excluded_fighters = ["Jon Jones", "Aspen Ladd"]
+excluded_fighter = "Jon Jones"
 
 
 def create_mma_p4p(division):
@@ -15,14 +15,12 @@ def create_mma_p4p(division):
         return find_fighter
 
     p4p_title = division.h4.text.strip()
-    print(p4p_title)
+
     ranked_first = division.h5.text.strip().split()
-    print(ranked_first)
+
     p4p_division = Division.objects.get(weight_class=p4p_title)
-    print(f"p4p_king: {p4p_division}")
 
     p4p_king = Fighter.objects.get(first_name=ranked_first[0], last_name=ranked_first[1])
-    print(f"p4p_king: {p4p_king}")
 
     p4p_king.p4p_ranking = 1
     p4p_king.save()
@@ -36,23 +34,24 @@ def create_mma_p4p(division):
         p4p_fighter_rank = p4p_ranks[p4p_fighters.index(fighter)].text.strip()
         p4p_ranked_fighter = fighter.text.strip()
         p4p_fighter_name = p4p_ranked_fighter.split()
+        p4p_first_name = p4p_fighter_name[0]
+        p4p_last_name = p4p_fighter_name[1]
 
-        if p4p_ranked_fighter == "Jon Jones":
-            # division_rank = "Not ranked"
-            #
-            # create_mma_fighter(p4p_fighter_name, division_rank, "Light Heavyweight")
+        if p4p_ranked_fighter == excluded_fighter:
+            division_rank = "Not ranked"
 
-            selected_fighter = get_fighter(p4p_fighter_name[0], p4p_fighter_name[1])
+            create_mma_fighter(p4p_fighter_name, division_rank, "Light Heavyweight")
+
+            selected_fighter = get_fighter(p4p_first_name, p4p_last_name)
 
         else:
             if len(p4p_fighter_name) > 2:
-                selected_fighter = get_fighter(p4p_fighter_name[0], ' '.join(p4p_fighter_name[1:]))
-                print(selected_fighter)
-            else:
-                selected_fighter = get_fighter(p4p_fighter_name[0], p4p_fighter_name[1])
-                print(selected_fighter)
+                selected_fighter = get_fighter(p4p_first_name, ' '.join(p4p_fighter_name[1:]))
 
-            print(selected_fighter)
+            else:
+                selected_fighter = get_fighter(p4p_first_name, p4p_last_name)
 
         selected_fighter.p4p_ranking = p4p_fighter_rank
         selected_fighter.save()
+
+        p4p_division.fighters.add(selected_fighter)
