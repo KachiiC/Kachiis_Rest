09d3e7fa45f40da4c11a3f05de11fd47d97ede48
Kachii <Kacheokcheong@gmail.com>
2021-03-15 18:55:36 +0000

    edit store name length

--- kach_backend_endpoints/backend_list/mma_backend/mma_store/mma_store_model.py
+++ kach_backend_endpoints/backend_list/mma_backend/mma_store/mma_store_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class StoreItem(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=250)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     product_thumbnail = models.CharField(max_length=500)
     product_link = models.CharField(max_length=250)
