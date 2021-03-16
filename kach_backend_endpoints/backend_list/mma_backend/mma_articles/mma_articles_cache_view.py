f1cbb399a5237fb6b639fd9097c8c82e9fa3e6cd
Kachii <Kacheokcheong@gmail.com>
2021-03-16 19:04:19 +0000

    changed mma articles to cache view

---
+++ kach_backend_endpoints/backend_list/mma_backend/mma_articles/mma_articles_cache_view.py
@@ -0,0 +1,39 @@
+from rest_framework import views
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from .mma_articles_model import Article
+from kach_backend_endpoints.management.url_webscrapper_lite import url_webscraper_lite
+from .mma_articles_serializer import ArticleSerializer
+from rest_framework.response import Response
+
+my_url = "https://www.mmaweekly.com/category/news"
+
+
+class MMAArticlesView(views.APIView):
+
+    @method_decorator(cache_page(60 * 60 * 24 * 7))
+    def get(self, request):
+        # Delete all objects
+        Article.objects.all().delete()
+
+        # Grabs Articles
+        page = url_webscraper_lite(my_url)
+        all_articles = page.findAll("div", "span6")
+        # Slice to only 10 articles
+        articles_slice = slice(0, 10)
+        latest_articles = all_articles[articles_slice]
+
+        for article in latest_articles:
+            Article(
+                title=article.h3.text.strip(),
+                post_time_stamp=article.find("span", "time").text,
+                post_description=article.p.text.strip(),
+                thumbnail_url=article.img["src"],
+                post_link=article.h3.a["href"]
+            ).save()
+
+        data = Article.objects.all()
+
+        serializer = ArticleSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
--- kach_backend_endpoints/urls.py
+++ kach_backend_endpoints/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .backend_list.hsk_api.hsk_view import hsk_words_list, hsk_single_word, all_hsk_levels, single_hsk_level
 from .backend_list.cards_api.card_view import cards_list
+from .backend_list.mma_backend.mma_articles.mma_articles_cache_view import MMAArticlesView
 from .backend_list.mma_backend.mma_dictionary.mma_dictionary_view import mma_dictionary_list
 from .backend_list.mma_backend.mma_techniques.mma_techniques_view import mma_techniques_list
 from .backend_list.mma_backend.mma_fighters.mma_fighter_view import mma_fighter_list, FeaturedFighterApiView
@@ -18,7 +19,8 @@ urlpatterns = [
     # Cards
     path("cards_list/", cards_list, name="cards_list"),
     # MMA Article List
-    path("mma_articles_list/", mma_articles_list, name="mma_articles_list"),
+    # path("mma_articles_list/", mma_articles_list, name="mma_articles_list"),
+    path("mma_articles_list", MMAArticlesView.as_view(), name="mma_articles_refresh"),
     # MMA Dictionary List
     path("mma_dictionary_list/", mma_dictionary_list, name="mma_dictionary_list"),
     # MMA Technique List
