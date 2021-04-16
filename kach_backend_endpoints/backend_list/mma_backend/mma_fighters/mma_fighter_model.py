fda7dfd90289ccc06e4c04e0fea8c58c9c84d6ca
Kachii <Kacheokcheong@gmail.com>
2021-04-16 23:21:01 +0100

    added mma legend files

--- kach_backend_endpoints/backend_list/mma_backend/mma_fighters/mma_fighter_model.py
+++ kach_backend_endpoints/backend_list/mma_backend/mma_fighters/mma_fighter_model.py
@@ -25,6 +25,7 @@ class Fighter(models.Model):
     wins_via_sub = models.IntegerField()
     wins_via_dec = models.IntegerField()
     status = models.CharField(max_length=100)
+    legend = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
---
+++ kach_backend_endpoints/management/commands/repop_mma_legends.py
@@ -0,0 +1,80 @@
+from django.core.management.base import BaseCommand
+from kach_backend_endpoints.management.url_webscrapper import url_webscraper
+from kach_backend_endpoints.management.repoppers.mma_p4p_repopper import create_mma_p4p
+from kach_backend_endpoints.backend_list.mma_backend.mma_divisions.mma_divisions_model import Division
+from kach_backend_endpoints.backend_list.mma_backend.mma_fighters.mma_fighter_model import Fighter
+from kach_backend_endpoints.management.repoppers.mma_fighter_repoppers import create_mma_fighter
+
+my_url = "https://www.ufc.com/rankings"
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # Delete all divisions and Fighters
+        Division.objects.all().delete()
+        Fighter.objects.all().delete()
+
+        # grabbing page
+        page = url_webscraper(my_url)
+        # grabs each division
+        rankings = page.findAll("table")[:-1]
+
+        for rank in rankings:
+            division_name = rank.h4.text.strip()
+
+            if division_name.split()[0] == "Women's":
+                division_gender = "Women"
+            else:
+                division_gender = "Men"
+
+            if division_name != "Men's Pound-for-Pound Top Rank" \
+                    and division_name != "Women's Pound-for-Pound Top Rank":
+
+                division_champion = rank.h5.text.strip().split()
+                create_mma_fighter(division_champion, "champion", division_name)
+
+                division_rankings = rank.findAll("tr")
+
+                for division_fighter in division_rankings:
+                    fighter_info = division_fighter.findAll("td")
+
+                    fighter_name = fighter_info[1].text.strip().split()
+
+                    fighter_rank = fighter_info[0].text
+
+                    create_mma_fighter(fighter_name, fighter_rank, division_name)
+
+            Division(
+                weight_class=division_name,
+                gender=division_gender,
+                pound_for_pound=False
+            ).save()
+
+            print(f"{division_name} added...")
+
+        else:
+            Division(
+                weight_class=division_name[:-9],
+                gender=division_gender,
+                pound_for_pound=True
+            ).save()
+
+        # Adds fighters to divisions
+        all_fighters = Fighter.objects.all()
+        all_divisions = Division.objects.all()
+
+        print("Adding fighters to correct divisions...")
+        for division in all_divisions:
+            for fighter in all_fighters:
+                if fighter.weight_class == division.weight_class:
+                    division.fighters.add(fighter)
+        print("All divisions added!")
+
+        print("Adding fighters to p4p_list...")
+        pound_for_pound_tables = [0, 9]
+
+        for division in pound_for_pound_tables:
+            create_mma_p4p(rankings[division])
+        print("p4p division added!")
+
+        print("Repop Complete!")
