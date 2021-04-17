7aa41a2b94bc660fa6410f2bb7f5a57d428a661d
Kachii <Kacheokcheong@gmail.com>
2021-04-17 16:19:28 +0100

    added highlights

--- kach_backend_endpoints/backend_list/mma_backend/mma_fighters/mma_fighter_model.py
+++ kach_backend_endpoints/backend_list/mma_backend/mma_fighters/mma_fighter_model.py
@@ -26,6 +26,7 @@ class Fighter(models.Model):
     wins_via_dec = models.IntegerField()
     status = models.CharField(max_length=100)
     legend = models.BooleanField(default=False)
+    highlights = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
